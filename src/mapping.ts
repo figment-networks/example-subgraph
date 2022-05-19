dad2d826faf902dadfc8c22073263a79ef78c259
Seb-figment <sebastien.nobert@figment.io>
2022-05-19 14:15:43 -0400

    update network + use cosmoshub-ts lib

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,23 +1,23 @@
 import { cosmos } from "@graphprotocol/graph-ts";
-import { MsgDelegate, MsgCoin, decodeMsgDelegate } from "./decoding";
+import { cosmos as cosmos_sdk } from "@graphprotocol/cosmoshub-ts";
 import { Delegation, Coin } from "../generated/schema";
 
 export function handleMsgDelegate(messageData: cosmos.MessageData): void {
-  const message = decodeMsgDelegate(messageData.message.value);
+  const message = cosmos_sdk.staking.v1beta1.decodeMsgDelegate(messageData.message.value);
   const id = `${messageData.block.header.hash.toHexString()}-${message.delegator_address}-${message.validator_address}`;
 
   saveDelegation(id, message);
 }
 
-function saveDelegation(id: string, message: MsgDelegate): void {
+function saveDelegation(id: string, message: cosmos_sdk.staking.v1beta1.MsgDelegate): void {
   const msg = new Delegation(id);
   msg.delegatorAddress = message.delegator_address;
   msg.validatorAddress = message.validator_address;
-  msg.amount = saveCoin(id, message.amount as MsgCoin);
+  msg.amount = saveCoin(id, message.amount as cosmos_sdk.base.v1beta1.Coin);
   msg.save();
 }
 
-function saveCoin(id: string, c: MsgCoin): string {
+function saveCoin(id: string, c: cosmos_sdk.base.v1beta1.Coin): string {
   const coin = new Coin(id);
   coin.amount = c.amount;
   coin.denom = c.denom;

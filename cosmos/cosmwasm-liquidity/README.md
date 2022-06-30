fb8ab4d3693b14fe95d28e42591c1594ff9da784
Seb-figment <sebastien.nobert@figment.io>
2022-06-30 11:58:11 -0400

    Add CosmWasm liquidity example

---
+++ cosmos/cosmwasm-liquidity/README.md
@@ -0,0 +1,3 @@
+# CosmWasm Liquidity Example
+
+This example subgraph stores `Contract` objects that represent cosmwasm contract. In order to do that, the handler function decode all messages `MsgExecuteContract` messages.
---
+++ cosmos/cosmwasm-liquidity/src/mapping.ts
@@ -0,0 +1,55 @@
+import { BigInt, cosmos, log } from "@graphprotocol/graph-ts";
+import { cosmwasm } from "@graphprotocol/juno-ts";
+import { Contract } from "../generated/schema";
+import { JSON } from "assemblyscript-json";
+
+export function handleMsgExecuteContract(messageData: cosmos.MessageData): void {
+  const message = cosmwasm.wasm.v1.decodeMsgExecuteContract(messageData.message.value);
+  const id = message.contract;
+  let contract = Contract.load(id);
+
+  if (contract == null) {
+    contract  = new Contract(id);
+    contract.liquidity = BigInt.fromI32(0);
+  }
+
+  const jsonMsg = <JSON.Obj>(JSON.parse(message.msg));
+
+  const add_liquidity = jsonMsg.getObj("add_liquidity");
+  if (add_liquidity != null) {
+    const amount = add_liquidity.getString("token1_amount");
+    if (amount != null) {
+      if (validateBigInt(amount.toString())) {
+        contract.liquidity = contract.liquidity.plus(BigInt.fromString(amount.toString()));
+      } else {
+        log.warning("amount in not a valid number: {}", [amount.toString()])
+      }
+    }
+  }
+
+  const remove_liquidity = jsonMsg.getObj("remove_liquidity");
+  if (remove_liquidity != null) {
+    const amount = remove_liquidity.getString("amount");
+    if (amount != null) {
+      if (validateBigInt(amount.toString())) {
+        contract.liquidity = contract.liquidity.minus(BigInt.fromString(amount.toString()));
+      } else {
+        log.warning("amount in not a valid number: {}", [amount.toString()])
+      }
+    }
+  }
+
+  contract.save();
+}
+
+function validateBigInt(str: string): boolean
+{
+  for(let i = 0; i < str.length; i++) {
+    const num = parseInt(str[i])
+    if (num == NaN) {
+      return false;
+    }
+  };
+
+  return true;
+}
